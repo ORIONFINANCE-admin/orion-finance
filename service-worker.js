6feb9a9633b28ba8930e2100d2c5ca78a1dc8891
ORIONFINANCE-admin <oriongroup.admin@gmail.com>
2026-04-08 22:45:30 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,11 @@
-const CACHE = "orion-v2"; // 🔥 muda versão aqui sempre que atualizar
+const CACHE = "orion-v3";
 
 self.addEventListener("install", e => {
   self.skipWaiting();
-
-  e.waitUntil(
-    caches.open(CACHE).then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/app.js"
-      ]);
-    })
-  );
 });
 
 self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(k => k !== CACHE)
-            .map(k => caches.delete(k))
-      );
-    })
-  );
-
   self.clients.claim();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
+self.addEventListener("fetch", () => {});
