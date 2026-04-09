db34c61a310cd08a9cd811b060f3c890a6d75143
ORIONFINANCE-admin <oriongroup.admin@gmail.com>
2026-04-08 21:19:38 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE = "orion-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/app.js"
+      ]);
+    })
+  );
+});
