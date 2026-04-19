a03f4bf93f33f826d3e28a641ea6f2b997520e11
ORIONFINANCE-admin <oriongroup.admin@gmail.com>
2026-04-18 23:17:52 -0300

    Atualizar o accounts.js

--- js/modules/accounts.js
+++ js/modules/accounts.js
@@ -1,33 +1,28 @@
 window.AccountsModule = (function(){
 
-  let accounts = DB.get("acc");
-  let transactions = DB.get("t");
-
   function init(){
 
-    if(accounts.length === 0){
-      accounts = [
+    if(!window.accounts || window.accounts.length === 0){
+      window.accounts = [
         { name: "Bradesco", initialBalance: 0, balance: 0, card: false },
         { name: "Banco Inter", initialBalance: 0, balance: 0, card: false },
         { name: "Mercado Pago", initialBalance: 0, balance: 0, card: true, type: "fake" },
         { name: "VR", initialBalance: 0, balance: 0, card: false }
       ];
 
-      DB.set("acc", accounts);
+      DB.set("acc", window.accounts);
     }
-
-    window.accounts = accounts;
   }
 
   function updateCache(){
 
     for (const k in ACCOUNT_CACHE) delete ACCOUNT_CACHE[k];
 
-    accounts.forEach(a=>{
+    window.accounts.forEach(a=>{
       ACCOUNT_CACHE[a.name] = a.initialBalance ?? a.balance ?? 0;
     });
 
-    transactions.forEach(t=>{
+    window.transactions.forEach(t=>{
       if(!ACCOUNT_CACHE.hasOwnProperty(t.account)) return;
 
       if(t.type === "entrada"){
